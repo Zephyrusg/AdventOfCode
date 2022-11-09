583d9cfacfc1d5333f79f048d9d2e078d4ddc41c
Bas de Koning <bas.dekoning@ogd.nl>
2022-11-09 21:22:38 +0100

    Datafiles

---
+++ AdventOfCode/2020/Day19.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2020
+{
+    internal class Day19
+    {
+
+        public static int Day19A() {
+            int test = 0;
+
+            string[] Data = File.ReadAllLines(".\\2020\\Input\\InputDay19Rules.txt");
+
+            Array.Sort(Data);
+            foreach (string Item in Data) {
+                Console.WriteLine(Item);
+            }
+            
+
+            return test;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -15,16 +15,17 @@ namespace AdventOfCode._2020 // Note: actual namespace depends on the project na
             //Console.WriteLine(Day16.Day16A());
             //Console.WriteLine(Day16.Day16B());
             //Console.WriteLine(Day17.Day17A());
-            Console.WriteLine(Day18.Day18A());
+            //Console.WriteLine(Day18.Day18A());
+            Console.WriteLine(Day19.Day19A());
             stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
             Console.WriteLine(ts);
-            stopWatch.Reset();
-            stopWatch.Start();
-            Console.WriteLine(Day18.Day18B());
-            stopWatch.Stop();
-            ts = stopWatch.Elapsed;
-            Console.WriteLine(ts);
+            //stopWatch.Reset();
+            //stopWatch.Start();
+            //Console.WriteLine(Day18.Day18B());
+            //stopWatch.Stop();
+            //ts = stopWatch.Elapsed;
+            //Console.WriteLine(ts);
             // Console.ReadLine();
         }
     }

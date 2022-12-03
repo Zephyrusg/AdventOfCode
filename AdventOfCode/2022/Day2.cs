15130d127728192cac5b97b71def73923ee82956
Bas de Koning <bas.dekoning@ogd.nl>
2022-12-03 07:37:00 +0100

    Day 1 2 3

--- AdventOfCode/2022/Day2.cs
+++ AdventOfCode/2022/Day2.cs
@@ -6,13 +6,99 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode._2022
 {
+
     internal class Day2
     {
-        static string Path = ".\\2022\\Input\\InputDay1.txt";
+
+        public static void FillCheatSheet() {
+            CheatSheet.Add("A X", 4);
+            CheatSheet.Add("A Y", 8);
+            CheatSheet.Add("A Z", 3);
+            CheatSheet.Add("B Y", 5);
+            CheatSheet.Add("B Z", 9);
+            CheatSheet.Add("B X", 1);
+            CheatSheet.Add("C Z", 6);
+            CheatSheet.Add("C X", 7);
+            CheatSheet.Add("C Y", 2);
+        }
+        public static Char GetNeededResult(Tuple<Char, Char> Play)
+        {
+            Char NeededResult = 'X';
+
+
+            switch (Play.Item2)
+            {
+                case 'X':
+
+                    if (Play.Item1 == 'A')
+                    {
+                        NeededResult = 'Z';
+                    }
+                    else if (Play.Item1 == 'B')
+                    {
+                        NeededResult = 'X';
+                    }
+                    else {
+                        NeededResult = 'Y';
+                    }
+                        
+                    break;
+
+                case 'Y':
+
+                    if (Play.Item1 == 'A')
+                    {
+                        NeededResult = 'X';
+                    }
+                    else if (Play.Item1 == 'B')
+                    {
+                        NeededResult = 'Y';
+                    }
+                    else
+                    {
+                        NeededResult = 'Z';
+                    }
+
+                    break;
+
+                case 'Z':
+                    if (Play.Item1 == 'A')
+                    {
+                        NeededResult = 'Y';
+                    }
+                    else if (Play.Item1 == 'B')
+                    {
+                        NeededResult = 'Z';
+                    }
+                    else
+                    {
+                        NeededResult = 'X';
+                    }
+                    break;
+
+            }
+
+            return NeededResult;
+        }
+        static Dictionary<string, int> CheatSheet = new Dictionary<string, int>();
+
+
+        static string Path = ".\\2022\\Input\\InputDay2.txt";
+
         public static int Part1()
         {
             int answer = 0;
-            string Data = File.ReadAllText(Path);
+
+            FillCheatSheet();
+
+
+            string[] Data = File.ReadAllLines(Path);
+
+            foreach (string Line in Data)
+            {
+            
+                answer += CheatSheet[Line];
+            }
 
             return answer;
         }
@@ -20,10 +106,24 @@ namespace AdventOfCode._2022
 
         public static int Part2()
         {
+            
             int answer = 0;
-            string Data = File.ReadAllText(Path);
+            string[] Data = File.ReadAllLines(Path);
+
+
+            foreach (string Line in Data)
+            {
+                Char[] Temp = new Char[] { };
+                Tuple<Char, Char> TestHand = Tuple.Create(Line[0], Line[2]);
+                Temp = Line.ToCharArray();
+                Temp[2] = GetNeededResult(TestHand);
+                string Result = new string(Temp);
+                answer += CheatSheet[Result];
+            }
 
             return answer;
         }
+
     }
+
 }
---
+++ AdventOfCode/2022/Day3.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2022
+{
+    internal class Day3
+    {
+        static string Path = ".\\2022\\Input\\InputDay3.txt";
+
+        public static char FindDouble(string s1, string s2)
+        {
+            
+            var Map = new HashSet<char>();
+
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (!Map.Contains(s1[i]))
+                {
+                    Map.Add(s1[i]);
+                }
+                
+            }
+
+            for (int i = 0; i < s2.Length; i++)
+            {
+                if (Map.Contains(s2[i]))
+                {
+                    return s2[i];
+                }
+                // else return 0
+            }
+            return '0'; 
+        }
+
+        public static char FindBadge(string s1, string s2, string s3) {
+
+
+            var Map = new HashSet<char>();
+
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (!Map.Contains(s1[i]))
+                {
+                    Map.Add(s1[i]);
+                }
+
+            }
+
+            foreach (char item in Map) { 
+                
+                if(s2.Contains(item) && s3.Contains(item))
+                {
+                    return item;
+                }
+            
+            } 
+
+            return '0';
+        }
+
+        public static int Part1()
+        {
+            string[] Data = File.ReadAllLines(Path);
+            int answer = 0;
+
+            foreach (string line in Data) {
+
+                int PriorityValue = 0;
+                string s1 = line.Substring(0, (int)(line.Length / 2));
+                string s2 = line.Substring((int)(line.Length / 2), (int)(line.Length / 2));
+                char package = FindDouble(s1, s2);
+
+                if (char.IsLower(package))
+                {
+                    PriorityValue = (int)package % 32;
+                }
+                else {
+                    PriorityValue = (int)package % 32 + 26;
+                }
+                answer += PriorityValue;
+                
+            }
+
+            return answer;
+        }
+
+        public static int Part2()
+        {
+            string[] Data = File.ReadAllLines(Path);
+            int answer = 0;
+
+            List<List<string>> AllGroups = new List<List<string>>();
+            int index = 0;
+            List<string> Group = new List<string>();
+            foreach (string line in Data) {
+                if (index < 3)
+                {
+                    Group.Add(line);
+                    index++;
+                }
+                else {
+                    index = 1;
+                    AllGroups.Add(Group);
+                    Group = new List<string>();
+                    Group.Add(line);
+                }
+
+            }
+            AllGroups.Add(Group);
+
+            foreach (List<string> GroupofBags in AllGroups) {
+                int PriorityValue = 0;
+                char Badge = FindBadge(GroupofBags[0], GroupofBags[1], GroupofBags[2]);
+                if (char.IsLower(Badge))
+                {
+                    PriorityValue = (int)Badge % 32;
+                }
+                else
+                {
+                    PriorityValue = (int)Badge % 32 + 26;
+                }
+                answer += PriorityValue;
+            }
+
+            
+
+
+            return answer;
+        }
+    }
+}

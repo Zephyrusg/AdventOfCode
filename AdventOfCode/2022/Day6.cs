ad1865df89a4eb176122b9395e6c3914df11f567
Bas de Koning <bas.dekoning@ogd.nl>
2022-12-06 07:20:53 +0100

    Day6

---
+++ AdventOfCode/2022/Day6.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2022
+{
+    internal class Day6
+    {
+
+        static string Path = ".\\2022\\Input\\InputDay6.txt";
+        public static int Part1()
+        {
+            string Data = File.ReadAllText(Path);
+            int answer = 0;
+
+            for (int i = 0; i < Data.Length; i++) {
+                
+                string Test = Data.Substring(i, 4);
+                if (Test.Distinct().Count() == 4) {
+                    answer = i + 4;
+                    break;
+                }    
+            }
+            return answer;
+        }
+        public static int Part2()
+        {
+            string Data = File.ReadAllText(Path);
+            int answer = 0;
+
+            for (int i = 0; i < Data.Length; i++)
+            {
+                string Test = Data.Substring(i, 14);
+
+                if (Test.Distinct().Count() == 14)
+                {
+                    answer = i + 14;
+                    break;
+                }
+            }
+            return answer;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -14,13 +14,13 @@ namespace AdventOfCode._2022 // Note: actual namespace depends on the project na
 
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
-            Console.WriteLine("Answer: " + Day5.Part1());
+            Console.WriteLine("Answer: " + Day6.Part1());
             stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
             Console.WriteLine("Completion Time: " + ts);
             stopWatch.Reset();
             stopWatch.Start();
-            Console.WriteLine("Answer: " + Day5.Part2());
+            Console.WriteLine("Answer: " + Day6.Part2());
             stopWatch.Stop();
             ts = stopWatch.Elapsed;
             Console.WriteLine("Completion Time: " + ts);

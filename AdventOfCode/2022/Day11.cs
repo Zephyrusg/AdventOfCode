d75e63843d93107568ed19709572979e5b330e2a
Bas de Koning <bas.dekoning@ogd.nl>
2022-12-10 21:05:07 +0100

    Rdy files for tomorrow

---
+++ AdventOfCode/2022/Day11.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2022
+{
+    internal class Day11
+    {
+        static string Path = ".\\2022\\Input\\InputDay11.txt";
+        public static int Part1()
+        {
+            string[] Data = File.ReadAllLines(Path);
+            int answer = 0;
+
+            return answer;
+        }
+
+   
+        public static int Part2()
+        {
+            string[] Data = File.ReadAllLines(Path);
+            int answer = 0;
+
+            return answer;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -14,13 +14,13 @@ namespace AdventOfCode._2022 // Note: actual namespace depends on the project na
 
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
-            Console.WriteLine("Answer: " + Day10.Part1());
+            Console.WriteLine("Answer: " + Day11.Part1());
             stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
             Console.WriteLine("Completion Time: " + ts);
             stopWatch.Reset();
             stopWatch.Start();
-            //Console.WriteLine("Answer: " + Day10.Part2());
+            Console.WriteLine("Answer: " + Day11.Part2());
             Day10.Part2();
             stopWatch.Stop();
             ts = stopWatch.Elapsed;
@@ -38,7 +38,7 @@ namespace AdventOfCode._2022 // Note: actual namespace depends on the project na
 
 //            Stopwatch stopWatch = new Stopwatch();
 //            stopWatch.Start();
-//            Console.WriteLine("Answer: " + Day19.Part1());
+//            //Console.WriteLine("Answer: " + Day19.Part1());
 //            stopWatch.Stop();
 //            TimeSpan ts = stopWatch.Elapsed;
 //            Console.WriteLine("Completion Time: " + ts);

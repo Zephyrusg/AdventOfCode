f24629c60c4ba95038d98bef92706cd61f551e06
Bas de Koning <bas.dekoning@ogd.nl>
2022-12-19 16:52:26 +0100

    Day 19

--- AdventOfCode/2022/Day19.cs
+++ AdventOfCode/2022/Day19.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Resources;
+using System.Runtime.Serialization;
 using System.Runtime.Versioning;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace AdventOfCode._2022
 {
@@ -14,31 +17,38 @@ namespace AdventOfCode._2022
 
         class BluePrint
         {
+           
             public int ID = 0;
             public int[] OreRobotCosts = new int[] { 0, 0, 0 };
             public int[] ClayRobotCosts = new int[] { 0, 0, 0 };
             public int[] ObsidianRobotCost = new int[] { 0, 0, 0 };
             public int[] GeodeRobotCost = new int[] { 0, 0, 0 };
+            public int OreMax;
+          
 
             public BluePrint(string BlueprintText)
             {
-                var Lines = BlueprintText.Split("\r\n");
-                ID  = Int32.Parse(Regex.Match(Lines[0], "\\d+").Value);
-                OreRobotCosts[0] = Int32.Parse(Regex.Match(Lines[1], "\\d+").Value);
-                ClayRobotCosts[0] = Int32.Parse(Regex.Match(Lines[2], "\\d+").Value);
-                ObsidianRobotCost[0] = Int32.Parse((Regex.Matches(Lines[3], "\\d+")[0]).Value);
-                ObsidianRobotCost[1] = Int32.Parse((Regex.Matches(Lines[3], "\\d+")[1]).Value);
-                GeodeRobotCost[0] = Int32.Parse((Regex.Matches(Lines[4], "\\d+")[0]).Value);
-                GeodeRobotCost[2] = Int32.Parse((Regex.Matches(Lines[4], "\\d+")[1]).Value);
+                var Digits = Regex.Matches(BlueprintText, "\\d+");
+                ID  = Int32.Parse(Digits[0].Value);
+                OreRobotCosts[0] = Int32.Parse(Digits[1].Value);
+                ClayRobotCosts[0] = Int32.Parse(Digits[2].Value);
+                ObsidianRobotCost[0] = Int32.Parse(Digits[3].Value);
+                ObsidianRobotCost[1] = Int32.Parse(Digits[4].Value);
+                GeodeRobotCost[0] = Int32.Parse(Digits[5].Value);
+                GeodeRobotCost[2] = Int32.Parse(Digits[6].Value);
+                OreMax = Math.Max(Math.Max(OreRobotCosts[0], ClayRobotCosts[0]), Math.Max(ObsidianRobotCost[0], GeodeRobotCost[0]));
 
+              
             }
 
 
         }
 
         class State {
-            public int[] Robots = new int[4];
-            public int[] Resources= new int[4];
+            public static List<int> MaxResultsOverTime= new List<int>();                
+            public static Dictionary<State,int> UniqueStates = new Dictionary<State, int>();
+            public int[] Robots;
+            public int[] Resources;
             public int Time;
 
             public State(int[] Robots, int[] Recources, int time) {
@@ -48,108 +58,191 @@ namespace AdventOfCode._2022
                 this.Time = time;
             
             }
-        }
-        static int DoSimulate(State CurrentState, BluePrint CurrentBuildPrint) {
 
-            int result = 0;
+            public override bool Equals(object? obj)
+            {
+                return obj is State state &&
+                       Robots[0] == state.Robots[0] &&
+                       Robots[1] == state.Robots[1] &&
+                       Robots[2] == state.Robots[0] &&
+                       Robots[3] == state.Robots[0] &&
+                       Resources[0] == state.Resources[0] &&
+                       Resources[1] == state.Resources[1] &&
+                       Resources[2] == state.Resources[2] &&
+                       Resources[3] == state.Resources[3] &&
+                       Time == state.Time;
+                       
+            }
+
+            public override int GetHashCode()
+            {
+                HashCode Hash = new HashCode();
+                Hash.Add(Robots[0]);
+                Hash.Add(Robots[1]);
+                Hash.Add(Robots[2]);
+                Hash.Add(Robots[3]);
+                Hash.Add(Resources[0]);
+                Hash.Add(Resources[1]);
+                Hash.Add(Resources[2]);
+                Hash.Add(Resources[3]);
+                Hash.Add(Time);
+                return Hash.ToHashCode();
+            }
+        }
+        static int DoSimulate(State CurrentState, BluePrint CurrentBuildPrint, int MaxTime)
+        {
+           
+            //if (State.UniqueStates.Contains(CurrentState))
+            //{
+
+            //    State Compare;
+            //    State.UniqueStates.TryGetValue(CurrentState, out Compare);
+            //    if (Compare.Time < CurrentState.Time)
+            //    {
+            //        return 0;
+            //    }
+            //    else if (Compare.Time > CurrentState.Time)
+            //    {
+
+            //        State.UniqueStates.Remove(CurrentState);
+            //        State.UniqueStates.Add(CurrentState);
+            //    }
+
+
+            //}
+            //else {
+            //    State.UniqueStates.Add(CurrentState);
+            //}
+
             int[] Robots = CurrentState.Robots;
             int[] Resources = CurrentState.Resources;
             int Time = CurrentState.Time;
-            Queue<State> ToCheck = new Queue<State>();
+            bool BuyGeodeRobot = false;
+            bool BuyObsidianRobot = false;
+            bool BuyOreRobot = false;
+            bool BuyClayRobot = false;
+            bool SomethingtoBuy = (new bool[]{ BuyGeodeRobot, BuyObsidianRobot, BuyOreRobot, BuyClayRobot}).Any(b=> b == true);
 
-            while (Time < 25)
+
+
+            if (Time == MaxTime)
             {
-                int[] ResourcesAtStart = (int[])Resources.Clone();
-                int[] TempResources = new int[] { 0, 0, 0, 0 };
-                int[] TempRobots = (int[])Robots.Clone();
+                return Resources[3];
+            }
+            //if (State.UniqueStates.ContainsKey(CurrentState))
+            //{
+            //    return State.UniqueStates[CurrentState];
+            //}
+            if (State.MaxResultsOverTime[Time] > CurrentState.Resources[3] + (CurrentState.Robots[3] + (MaxTime - Time)) * (MaxTime - Time))
+            {
+                return 0;
+            }
+            int[] Addition = new int[] { Robots[0], Robots[1], Robots[2], Robots[3] };
 
-
-                TempResources[0] = Robots[0] * 1;
-                TempResources[1] = Robots[1] * 1;
-                TempResources[2] = Robots[2] * 1;
-                TempResources[3] = Robots[3] * 1;
-
-
-                if (CurrentBuildPrint.GeodeRobotCost[0] <= Resources[0] && CurrentBuildPrint.GeodeRobotCost[2] <= Resources[2])
-                {
-                    Robots[3]++;
-                    Resources[2] -= CurrentBuildPrint.GeodeRobotCost[2];
-                    Resources[0] -= CurrentBuildPrint.GeodeRobotCost[0];
-
-                }
-                else if ((CurrentBuildPrint.ObsidianRobotCost[0] <= Resources[0]) && (CurrentBuildPrint.ObsidianRobotCost[1] <= Resources[1]))
-                {
-                    Robots[2]++;
-                    Resources[1] -= CurrentBuildPrint.ObsidianRobotCost[1];
-                    Resources[0] -= CurrentBuildPrint.ObsidianRobotCost[0];
-                }
-                else if (CurrentBuildPrint.ClayRobotCosts[0] <= Resources[0])
-                {
-                    Robots[1]++;
-                    Resources[0] -= CurrentBuildPrint.ClayRobotCosts[0];
-                }
-
-                else if (CurrentBuildPrint.OreRobotCosts[0] <= Resources[0])
-                {
-
-                    Robots[0]++;
-                    Resources[0] -= CurrentBuildPrint.OreRobotCosts[0];
-                }
-
-                Resources[0] += TempResources[0];
-                Resources[1] += TempResources[1];
-                Resources[2] += TempResources[2];
-                Resources[3] += TempResources[3];
-
-                if (Robots.Sum() > TempRobots.Sum())
-                {
-                    ResourcesAtStart[0] += TempResources[0];
-                    ResourcesAtStart[1] += TempResources[1];
-                    ResourcesAtStart[2] += TempResources[2];
-                    ResourcesAtStart[3] += TempResources[3];
-                    Time++;
-                    ToCheck.Enqueue(new State(TempRobots, ResourcesAtStart, Time));
-
-                }
-                else {
-                    Time++;
-                }
-
-
+            List<int> NumberOfMaxGeode = new List<int>();
+            NumberOfMaxGeode.Add(0);
+            if (CurrentBuildPrint.OreRobotCosts[0] <= Resources[0] && (Resources[0] < (MaxTime - Time) * CurrentBuildPrint.OreMax))
+            {
+                BuyOreRobot = true;
 
             }
-            if (ToCheck.Count > 0){ 
-                while (ToCheck.Count > 0) {
-
-                    State State = ToCheck.Dequeue();
-                    result = Math.Max(Resources[3], DoSimulate(State, CurrentBuildPrint));
-
-                }
+            if (CurrentBuildPrint.ClayRobotCosts[0] <= Resources[0] && (Resources[1] < (MaxTime - Time) * CurrentBuildPrint.ObsidianRobotCost[1]))
+            {
+                BuyClayRobot = true;
             }
-            return Math.Max(Resources[3],result);
+            if ((CurrentBuildPrint.ObsidianRobotCost[0] <= Resources[0]) && (CurrentBuildPrint.ObsidianRobotCost[1] <= Resources[1]) && (Resources[2] < (MaxTime - Time) * CurrentBuildPrint.GeodeRobotCost[2]))
+            {
+                BuyObsidianRobot = true;
+            }
+
+            if (CurrentBuildPrint.GeodeRobotCost[0] <= Resources[0] && CurrentBuildPrint.GeodeRobotCost[2] <= Resources[2])
+            {
+                BuyGeodeRobot = true;
+            }
+           
+            
+
+            if (BuyGeodeRobot)
+            {
+
+                State State = new State(
+
+                    new int[] { Robots[0], Robots[1], Robots[2], Robots[3] + 1 },
+                    new int[] { Resources[0] - CurrentBuildPrint.GeodeRobotCost[0] + Addition[0], Resources[1] + Addition[1], Resources[2] - CurrentBuildPrint.GeodeRobotCost[2] + Addition[2], Resources[3] + Addition[3] },
+                    Time + 1
+                    );
+                NumberOfMaxGeode.Add(DoSimulate(State, CurrentBuildPrint, MaxTime));
+
+            }
+            else
+            {
+
+                if (BuyObsidianRobot)
+                {
+                    State State = new State(
+                        new int[] { Robots[0], Robots[1], Robots[2] + 1, Robots[3] },
+                        new int[] { Resources[0] - CurrentBuildPrint.ObsidianRobotCost[0] + Addition[0], Resources[1] - CurrentBuildPrint.ObsidianRobotCost[1] + Addition[1], Resources[2] + Addition[2], Resources[3] + Addition[3] },
+                        Time + 1
+                        );
+                    NumberOfMaxGeode.Add(DoSimulate(State, CurrentBuildPrint, MaxTime));
+                }
+                if (BuyClayRobot)
+                {
+                    State State = new State(
+                        new int[] { Robots[0], Robots[1] + 1, Robots[2], Robots[3] },
+                        new int[] { Resources[0] - CurrentBuildPrint.ClayRobotCosts[0] + Addition[0], Resources[1] + Addition[1], Resources[2] + Addition[2], Resources[3] + Addition[3] },
+                        Time + 1
+                        );
+                    NumberOfMaxGeode.Add(DoSimulate(State, CurrentBuildPrint, MaxTime));
+                }
+
+                if (BuyOreRobot)
+                {
+                    State State = new(
+                        new int[] { Robots[0] + 1, Robots[1], Robots[2], Robots[3] },
+                        new int[] { Resources[0] - CurrentBuildPrint.OreRobotCosts[0] + Addition[0], Resources[1] + Addition[1], Resources[2] + Addition[2], Resources[3] + Addition[3] },
+                        Time + 1
+                        );
+                    NumberOfMaxGeode.Add(DoSimulate(State, CurrentBuildPrint, MaxTime));
+                }
+                if (Resources[0] <= CurrentBuildPrint.OreMax)
+                {
+                    State State = new State(
+                        (int[])Robots.Clone(),
+                        new int[] { Resources[0] + Addition[0], Resources[1] + Addition[1], Resources[2] + Addition[2], Resources[3] + Addition[3] },
+                        Time + 1);
+                    NumberOfMaxGeode.Add(DoSimulate(State, CurrentBuildPrint, MaxTime));
+                }
+            }        
+            int result = NumberOfMaxGeode.Max();
+            if (State.MaxResultsOverTime[Time] < CurrentState.Resources[3])
+                State.MaxResultsOverTime[Time] = CurrentState.Resources[3];
+            //State.UniqueStates[CurrentState] = result;
+            return result;
+            
         }
-
         static string Path = ".\\2022\\Input\\InputDay19.txt";
-        static string Data = File.ReadAllText(Path);
+        static string[] Data = File.ReadAllLines(Path);
         public static int Part1()
         {
             int answer = 0;
 
             //Bueprints in laden
-            var BluePrints = Data.Split("\r\n\r\n");
-            foreach (var BluePrintText in BluePrints)
+         
+            foreach (var line in Data)
             {
-                BluePrint CurrentBuildPrint = new BluePrint(BluePrintText);
+                BluePrint CurrentBuildPrint = new BluePrint(line);
+                //State.UniqueStates = new Dictionary<State, int>();
 
                 int[] Robots = new int[] { 1, 0, 0, 0 };
                 int[] Resources = new int[] { 0, 0, 0, 0 };
 
-                State StartState = new State(Robots, Resources, 1);
+                State StartState = new State(Robots, Resources, 0);
+                State.MaxResultsOverTime = new List<int>(new int[24]);
 
-
-                int Result = DoSimulate(StartState, CurrentBuildPrint);
+                int Result = DoSimulate(StartState, CurrentBuildPrint, 24);
                 answer += Result * CurrentBuildPrint.ID;
-
+                Console.WriteLine("Blueprint: " + CurrentBuildPrint.ID + " With Result: " + Result + " Done.");
 
             }
 
@@ -162,6 +255,27 @@ namespace AdventOfCode._2022
         public static int Part2()
         {
             int answer = 0;
+            var lines = Data.Take(3);
+            int[] Result = new int[3];
+            foreach (var line in lines)
+            {
+                BluePrint CurrentBuildPrint = new BluePrint(line);
+
+                int[] Robots = new int[] { 1, 0, 0, 0 };
+                int[] Resources = new int[] { 0, 0, 0, 0 };
+
+                State StartState = new State(Robots, Resources, 0);
+                State.MaxResultsOverTime = new List<int>(new int[32]);
+                
+                Result[CurrentBuildPrint.ID - 1] = DoSimulate(StartState, CurrentBuildPrint, 32);
+                Console.WriteLine("Blueprint: " + CurrentBuildPrint.ID + " With Result: " + Result[CurrentBuildPrint.ID - 1] + " Done.");
+                
+                
+                State.UniqueStates = new Dictionary<State, int>();
+
+            }
+            answer = Result[0] * Result[1] * Result[2];
+
 
             return answer;
         }

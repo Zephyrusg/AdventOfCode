6d711113b90354aa8702246907dc955628540c93
Bas de Koning <bas.dekoning@ogd.nl>
2022-12-23 07:24:01 +0100

    Moving to Work

--- AdventOfCode/2022/Day22.cs
+++ AdventOfCode/2022/Day22.cs
@@ -14,160 +14,270 @@ namespace AdventOfCode._2022
         enum Field
         {
             Path, Wall
-        
         };
         
         static HashSet<(int x,int y)> AllLoctions = new HashSet<(int x,int y)> ();
         static Dictionary<(int x, int y), Field> AllLocationFields = new Dictionary<(int x, int y), Field>();
+        static Dictionary<(int x, int y), int> SiteCollection = new Dictionary<(int x, int y), int>();
+        static int Direction = 0;
         static string Path = ".\\2022\\Input\\InputDay22.txt";
         static string Data = File.ReadAllText(Path);
+        static bool Wall = false;
+        static bool Firststar = true;
 
-        static (int x, int y) MoveRight((int x, int y) CurrentPosition, int Steps){
+        static (int x, int y) GetNextMove((int x, int y) CurrentPosition ){
+            int site = SiteCollection[CurrentPosition];
+            (int x, int y) NextPosition = (0,0);
 
-            while(Steps != 0)
+            if (site == 1)
             {
-                (int x, int y) Next = (CurrentPosition.x + 1, CurrentPosition.y);
-                if (AllLoctions.Contains(Next)) 
+                if (Direction == 0)
                 {
-                    if (AllLocationFields[Next] != Field.Wall)
-                    {
+                    Direction = 90;
+                    NextPosition = (0, CurrentPosition.x + 100);
+                }
+                else if (Direction == 270)
+                {
+                    Direction = 90;
+                    NextPosition = (0, 149 - CurrentPosition.y);
+                }
+            }
+            else if (site == 2)
+            {
+                if (Direction == 0)
+                {
+                    Direction = 0;
+                    NextPosition = (CurrentPosition.x - 100, 199);
+                }
+                else if (Direction == 90)
+                {
+                    Direction = 270;
+                    NextPosition = (99, 149 - CurrentPosition.y);
+                }
+                else if (Direction == 180)
+                {
+                    Direction = 270;
+                    NextPosition = (99, CurrentPosition.x - 50);
+                }
+            }
+            else if (site == 3)
+            {
+                if (Direction == 90)
+                {
+                    Direction = 0;
+                    NextPosition = (CurrentPosition.y + 50, 49);
+                }
+                else if (Direction == 270)
+                {
+                    Direction = 180;
+                    NextPosition = (CurrentPosition.y - 50, 100);
+                }
+            }
+            else if (site == 4)
+            {
+                if (Direction == 90)
+                {
+                    Direction = 270;
+                    NextPosition = (149, 149 - CurrentPosition.y);
+                }
+                else if (Direction == 180)
+                {
+                    Direction = 270;
+                    NextPosition = (49, CurrentPosition.x + 100);
+                }
+            }
+            else if (site == 5)
+            {
+                if (Direction == 0)
+                {
+                    Direction = 90;
+                    NextPosition = (50, CurrentPosition.x + 50);
+                }
+                else if (Direction == 270)
+                {
+                    Direction = 90;
+                    NextPosition = (50, 149 - CurrentPosition.y);
+                }
 
-                        CurrentPosition = Next;
-                    }
-                    else {
-                        break;
-                    }
+            }
+            else if (site == 6)
+            {
+                if (Direction == 90)
+                {
+                    Direction = 0;
+                    NextPosition = (CurrentPosition.y - 100, 149);
+
+                }
+                else if (Direction == 180)
+                {
+                    Direction = 180;
+                    NextPosition = (CurrentPosition.x + 100, 0);
+                }
+                else if (Direction == 270)
+                {
+                    Direction = 180;
+                    NextPosition = (CurrentPosition.y - 100, 0);
+                }
+            }
+            else {
+                Console.WriteLine("Error Site: " + site + " x: " + CurrentPosition.x + " y: " + CurrentPosition.y);
+            }
+            
+            return NextPosition;
+        
+        }
+        static (int x, int y) MoveRight((int x, int y) CurrentPosition){
+
+            (int x, int y) Next = (CurrentPosition.x + 1, CurrentPosition.y);
+            if (AllLoctions.Contains(Next)) 
+            {
+                if (AllLocationFields[Next] != Field.Wall)
+                {
+                    CurrentPosition = Next;
                 }
                 else {
-                    Next = (AllLoctions.Where(loc => loc.y == CurrentPosition.y).MinBy(loc => loc.x));
-                    if (AllLocationFields[Next] != Field.Wall)
-                    {
-
-                        CurrentPosition = Next;
-                    }
-                    else
-                    {
-                        break;
-                    }
+                    Wall = true;
                 }
-
-                Steps--;
             }
-            return CurrentPosition;
-        }
-        static (int x, int y) MoveLeft((int x, int y) CurrentPosition, int Steps)
-        {
-
-            while (Steps != 0)
-            {
-                (int x, int y) Next = (CurrentPosition.x - 1, CurrentPosition.y);
-                if (AllLoctions.Contains(Next))
+            else {
+                if (Firststar)
                 {
-                    if (AllLocationFields[Next] != Field.Wall)
-                    {
-
-                        CurrentPosition = Next;
-                    }
-                    else
-                    {
-                        break;
-                    }
+                    Next = (AllLoctions.Where(loc => loc.y == CurrentPosition.y).MinBy(loc => loc.x));
+                }
+                else 
+                {
+                    Next = GetNextMove(CurrentPosition);
+                }
+                
+                if (AllLocationFields[Next] != Field.Wall)
+                {
+                    CurrentPosition = Next;
                 }
                 else
+                {
+                    Wall = true;
+                }
+            }            
+            return CurrentPosition;
+        }
+        static (int x, int y) MoveLeft((int x, int y) CurrentPosition)
+        {
+
+            
+            (int x, int y) Next = (CurrentPosition.x - 1, CurrentPosition.y);
+            if (AllLoctions.Contains(Next))
+            {
+                if (AllLocationFields[Next] != Field.Wall)
+                {
+                    CurrentPosition = Next;
+                }
+                else
+                {
+                    Wall = true;
+                }
+            }
+            else
+            {
+                if (Firststar)
                 {
                     Next = (AllLoctions.Where(loc => loc.y == CurrentPosition.y).MaxBy(loc => loc.x));
-                    if (AllLocationFields[Next] != Field.Wall)
-                    {
-
-                        CurrentPosition = Next;
-                    }
-                    else
-                    {
-                        break;
-                    }
                 }
-
-                Steps--;
-            }
-            return CurrentPosition;
-        }
-        static (int x, int y) MoveUp((int x, int y) CurrentPosition, int Steps)
-        {
-
-            while (Steps != 0)
-            {
-                (int x, int y) Next = (CurrentPosition.x, CurrentPosition.y - 1);
-                if (AllLoctions.Contains(Next))
+                else { 
+                    Next = GetNextMove(CurrentPosition);
+                }
+                if (AllLocationFields[Next] != Field.Wall)
                 {
-                    if (AllLocationFields[Next] != Field.Wall)
-                    {
-
-                        CurrentPosition = Next;
-                    }
-                    else
-                    {
-                        break;
-                    }
+                    CurrentPosition = Next;
                 }
                 else
+                {
+                    Wall = true;
+                }
+            }
+                
+            return CurrentPosition;
+        }
+        static (int x, int y) MoveUp((int x, int y) CurrentPosition)
+        {
+
+            (int x, int y) Next = (CurrentPosition.x, CurrentPosition.y - 1);
+            if (AllLoctions.Contains(Next))
+            {
+                if (AllLocationFields[Next] != Field.Wall)
+                {
+                    CurrentPosition = Next;
+                }
+                else
+                {
+                    Wall = true;
+                }
+            }
+            else
+            {
+                if (Firststar)
                 {
                     Next = (AllLoctions.Where(loc => loc.x == CurrentPosition.x).MaxBy(loc => loc.y));
-                    if (AllLocationFields[Next] != Field.Wall)
-                    {
+                }
+                else 
+                { 
 
-                        CurrentPosition = Next;
-                    }
-                    else
-                    {
-                        break;
-                    }
+                    Next= GetNextMove(CurrentPosition);
                 }
 
-                Steps--;
-            }
-            return CurrentPosition;
-        }
-        static (int x, int y) MoveDown((int x, int y) CurrentPosition, int Steps)
-        {
-
-            while (Steps != 0)
-            {
-                (int x, int y) Next = (CurrentPosition.x, CurrentPosition.y + 1);
-                if (AllLoctions.Contains(Next))
+                if (AllLocationFields[Next] != Field.Wall)
                 {
-                    if (AllLocationFields[Next] != Field.Wall)
-                    {
-
-                        CurrentPosition = Next;
-                    }
-                    else
-                    {
-                        break;
-                    }
+                    CurrentPosition = Next;
                 }
                 else
                 {
-                    Next = (AllLoctions.Where(loc => loc.x == CurrentPosition.x).MinBy(loc => loc.y));
-                    if (AllLocationFields[Next] != Field.Wall)
-                    {
-
-                        CurrentPosition = Next;
-                    }
-                    else
-                    {
-                        break;
-                    }
+                    Wall = true;
                 }
-
-                Steps--;
             }
+          
+            return CurrentPosition;
+        }
+        static (int x, int y) MoveDown((int x, int y) CurrentPosition)
+        {
+
+          
+            (int x, int y) Next = (CurrentPosition.x, CurrentPosition.y + 1);
+            if (AllLoctions.Contains(Next))
+            {
+                if (AllLocationFields[Next] != Field.Wall)
+                {
+                    CurrentPosition = Next;
+                }
+                else
+                {
+                    Wall = true;
+                }
+            }
+            else
+            {
+                if (Firststar)
+                {
+                    Next = (AllLoctions.Where(loc => loc.x == CurrentPosition.x).MinBy(loc => loc.y));
+                }
+                else
+                {
+                    Next= GetNextMove(CurrentPosition);
+                }
+                if (AllLocationFields[Next] != Field.Wall)
+                {
+                    CurrentPosition = Next;
+                }
+                else
+                {
+                    Wall = true;
+                }
+            }
+
             return CurrentPosition;
         }
 
 
         public static int Part1()
         {
-            int answer = 0;
 
             string MapText = Data.Split("\r\n\r\n")[0];
             string Directions = Data.Split("\r\n\r\n")[1];
@@ -177,7 +287,6 @@ namespace AdventOfCode._2022
             for (int y = 0; y < Map.Length; y++)
             {
                 for (int x = 0; x < Map[y].Length; x++) {
-                
 
                     switch (Map[y][x])
                     {
@@ -187,7 +296,8 @@ namespace AdventOfCode._2022
                                 AllLoctions.Add((x, y));
                                 break;
                             }
-                        case '#': {
+                        case '#':
+                            {
                                 AllLocationFields.Add((x, y), Field.Wall);
                                 AllLoctions.Add((x, y));
                                 break;
@@ -197,13 +307,15 @@ namespace AdventOfCode._2022
                                 break;
                             }
                     }
+
                 }
             }
-            int Direction = 0;
+            Direction = 0;
 
             Directions = 'R' + Directions;
             List<string> DirectionMatches = Regex.Matches(Directions, "(R|L)\\d{1,2}").Select(M=>M.Value).ToList();
             int Steps = 0;
+            int Count =0;
             (int x, int y) CurrentLocation = AllLoctions.Where(loc => loc.y == 0).MinBy(loc => loc.x);
             foreach(string Move in DirectionMatches) {
 
@@ -223,30 +335,35 @@ namespace AdventOfCode._2022
                     }
                     
                 }
+                Wall = false;
                 Steps = int.Parse(Regex.Match(Move, "\\d{1,2}").Value);
-                switch(Direction)
-                {
-                    case 0:
-                        {
-                            CurrentLocation = MoveUp(CurrentLocation, Steps); 
-                            break;
-                        }
-                    case 90:
-                        {
-                            CurrentLocation = MoveRight(CurrentLocation, Steps);
-                            break;
-                        }
-                    case 180:
-                        {
-                            CurrentLocation = MoveDown(CurrentLocation, Steps);
-                            break;
-                        }
-                    case 270:
-                        { 
-                            CurrentLocation = MoveLeft(CurrentLocation, Steps);
-                            break; 
-                        }
+                while(Steps > 0 && Wall == false){
+                    switch (Direction)
+                    {
+                        case 0:
+                            {
+                                CurrentLocation = MoveUp(CurrentLocation);
+                                break;
+                            }
+                        case 90:
+                            {
+                                CurrentLocation = MoveRight(CurrentLocation);
+                                break;
+                            }
+                        case 180:
+                            {
+                                CurrentLocation = MoveDown(CurrentLocation);
+                                break;
+                            }
+                        case 270:
+                            {
+                                CurrentLocation = MoveLeft(CurrentLocation);
+                                break;
+                            }
+                    }
+                    Steps--;
                 }
+                Count++;
             }
 
             if (Direction == 0)
@@ -272,9 +389,118 @@ namespace AdventOfCode._2022
    
         public static int Part2()
         {
-            int answer = 0;
+            string MapText = Data.Split("\r\n\r\n")[0];
+            string Directions = Data.Split("\r\n\r\n")[1];
+            Firststar = false;
+            string[] Map = MapText.Split("\r\n");
 
-            return answer;
+            for (int y = 0; y < Map.Length; y++)
+            {
+                for (int x = 0; x < Map[y].Length; x++)
+                {
+
+
+                    if ((x >= 50 && x <= 99) && (y >= 0 && y <= 49))
+                    {
+                        SiteCollection.Add((x, y), 1);
+                    }
+                    else if (x >= 100 && x <= 149 && y >= 0 && y <= 49)
+                    {
+                        SiteCollection.Add((x, y), 2);
+                    }
+                    else if (x >= 50 && x <= 99 && y >= 50 && y <= 99)
+                    {
+                        SiteCollection.Add((x, y), 3);
+                    }
+                    else if (x >= 50 && x <= 99 && y >= 100 && y <= 149)
+                    {
+                        SiteCollection.Add((x, y), 4);
+                    }
+                    else if (x >= 0 && x <= 49 && y >= 100 && y <= 149)
+                    {
+                        SiteCollection.Add((x, y), 5);
+                    }
+                    else if (x >= 0 && x <= 49 && y >= 150 && y <= 199)
+                    {
+                        SiteCollection.Add((x, y), 6);
+                    }
+                }
+            }
+            Direction = 0;
+
+            Directions = 'R' + Directions;
+            List<string> DirectionMatches = Regex.Matches(Directions, "(R|L)\\d{1,2}").Select(M => M.Value).ToList();
+            int Steps = 0;
+            (int x, int y) CurrentLocation = AllLoctions.Where(loc => loc.y == 0).MinBy(loc => loc.x);
+            foreach (string Move in DirectionMatches)
+            {
+
+                if (Move[0] == 'R')
+                {
+                    Direction += 90;
+                    if (Direction == 360)
+                    {
+                        Direction = 0;
+                    }
+                }
+                else
+                {
+                    Direction -= 90;
+                    if (Direction < 0)
+                    {
+                        Direction = 270;
+                    }
+
+                }
+                Wall = false;
+                Steps = int.Parse(Regex.Match(Move, "\\d{1,2}").Value);
+                while (Steps > 0 && Wall == false)
+                {
+                    switch (Direction)
+                    {
+                        case 0:
+                            {
+                                CurrentLocation = MoveUp(CurrentLocation);
+                                break;
+                            }
+                        case 90:
+                            {
+                                CurrentLocation = MoveRight(CurrentLocation);
+                                break;
+                            }
+                        case 180:
+                            {
+                                CurrentLocation = MoveDown(CurrentLocation);
+                                break;
+                            }
+                        case 270:
+                            {
+                                CurrentLocation = MoveLeft(CurrentLocation);
+                                break;
+                            }
+                    }
+                    Steps--;
+                }
+            }
+
+            if (Direction == 0)
+            {
+                Direction = 3;
+            }
+            else if (Direction == 90)
+            {
+                Direction = 0;
+            }
+            else if (Direction == 180)
+            {
+                Direction = 1;
+            }
+            else
+            {
+                Direction = 2;
+            }
+
+            return ((1000 * (CurrentLocation.y + 1)) + (4 * (CurrentLocation.x + 1) + Direction));
         }
     }
 }
---
+++ AdventOfCode/2022/Day23.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2022
+{
+    
+internal class Day23
+    {
+        class Elf
+        {
+            public static HashSet<(int x, int y)> AllElfsLocations = new HashSet<(int x, int y)>();
+            public static HashSet<(int x, int y)> ValidProposedMoves = new HashSet<(int x, int y)>();
+            public static Queue<char> ToProposMoves = new Queue<char>(new List<char> { 'N', 'S', 'W', 'E' });
+            public (int x, int y) CurrentLocation;
+            public (int x, int y) ?ProposedLocation = null;
+
+
+            public Elf((int x, int y) currentLocation)
+            {
+                CurrentLocation = currentLocation;
+                AllElfsLocations.Add(currentLocation);
+            }
+
+            public bool CanMove() {
+
+                if (
+                    (!AllElfsLocations.Contains((CurrentLocation.x,     CurrentLocation.y + 1))) &&
+                    (!AllElfsLocations.Contains((CurrentLocation.x - 1, CurrentLocation.y + 1))) &&
+                    (!AllElfsLocations.Contains((CurrentLocation.x + 1, CurrentLocation.y + 1))) &&
+                    (!AllElfsLocations.Contains((CurrentLocation.x,     CurrentLocation.y - 1))) &&
+                    (!AllElfsLocations.Contains((CurrentLocation.x - 1, CurrentLocation.y + 1))) &&
+                    (!AllElfsLocations.Contains((CurrentLocation.x + 1, CurrentLocation.y + 1))) &&
+                    (!AllElfsLocations.Contains((CurrentLocation.x - 1, CurrentLocation.y    ))) &&
+                    (!AllElfsLocations.Contains((CurrentLocation.x + 1, CurrentLocation.y    )))
+                    ){
+                    ProposedLocation = null;
+                    return false;
+                }
+
+                return true;
+            }
+            public void ProposeMove() {
+
+                List<char> OrderProposeMoves = ToProposMoves.ToList();
+                int i = 0;
+                bool ProposedLocation = false; 
+                while (!ProposedLocation) {
+                    switch(OrderProposeMoves[i]) {
+                        case 'N': {
+                                if (
+                                    (!AllElfsLocations.Contains((CurrentLocation.x,     CurrentLocation.y - 1))) &&
+                                    (!AllElfsLocations.Contains((CurrentLocation.x - 1, CurrentLocation.y - 1))) &&
+                                    (!AllElfsLocations.Contains((CurrentLocation.x + 1, CurrentLocation.y - 1)))) 
+                                {
+                                    break;
+                                }
+                                this.ProposedLocation = (CurrentLocation.x, CurrentLocation.y - 1);
+                                ProposedLocation = true;
+                                break;
+                            }
+                        case 'S':
+                            {
+                                if (
+                                    (!AllElfsLocations.Contains((CurrentLocation.x,     CurrentLocation.y + 1))) &&
+                                    (!AllElfsLocations.Contains((CurrentLocation.x - 1, CurrentLocation.y + 1))) &&
+                                    (!AllElfsLocations.Contains((CurrentLocation.x + 1, CurrentLocation.y + 1))))
+                                {
+                                    break;
+                                }
+                                this.ProposedLocation = (CurrentLocation.x, CurrentLocation.y + 1);
+                                ProposedLocation = true;
+                                break;
+                            }
+                        case 'W':
+                            {
+                                if (
+                                    (!AllElfsLocations.Contains((CurrentLocation.x - 1, CurrentLocation.y - 1))) &&
+                                    (!AllElfsLocations.Contains((CurrentLocation.x - 1, CurrentLocation.y    ))) &&
+                                    (!AllElfsLocations.Contains((CurrentLocation.x - 1, CurrentLocation.y + 1))))
+                                {
+                                    break;
+                                }
+                                this.ProposedLocation = (CurrentLocation.x - 1, CurrentLocation.y);
+                                ProposedLocation = true;
+                                break;
+                            }
+                        case 'E':
+                            {
+                                if (
+                                    (!AllElfsLocations.Contains((CurrentLocation.x + 1, CurrentLocation.y + 1))) &&
+                                    (!AllElfsLocations.Contains((CurrentLocation.x + 1, CurrentLocation.y    ))) &&
+                                    (!AllElfsLocations.Contains((CurrentLocation.x + 1, CurrentLocation.y - 1))))
+                                {
+                                    break;
+                                }
+                                this.ProposedLocation = (CurrentLocation.x + 1, CurrentLocation.y);
+                                ProposedLocation = true;
+                                break;
+                            }
+                    }
+                    i++;
+                }
+                if (!ValidProposedMoves.Contains(((int x, int y))this.ProposedLocation))
+                {
+                    ValidProposedMoves.Add(((int x, int y))this.ProposedLocation);
+                }
+                else { 
+                    ValidProposedMoves.Remove(((int x, int y))this.ProposedLocation);
+                }
+                
+            }
+            public void ProcessMove() {
+                if (ValidProposedMoves.Contains(((int x, int y))this.ProposedLocation)){
+                    CurrentLocation = ((int x, int y))this.ProposedLocation;
+                    AllElfsLocations.Add(CurrentLocation);
+                }
+                else
+                {
+                    AllElfsLocations.Add(CurrentLocation);
+                }
+                ProposedLocation = null;
+            }
+        }
+        static List<Elf> AllElfs = new List<Elf>();
+        static string Path = ".\\2022\\Input\\InputDay23.txt";
+        static string Data = File.ReadAllText(Path);
+        public static int Part1()
+        {
+           
+            int answer = 0;
+
+            return answer;
+        }
+
+   
+        public static int Part2()
+        {
+            int answer = 0;
+
+            return answer;
+        }
+    }
+}

882e91cac0059bc87047dfe8ef1aef40a56559d3
Bas de Koning <bas.dekoning@ogd.nl>
2022-12-27 10:02:29 +0100

    Day 24 again

--- AdventOfCode/2022/Day24.cs
+++ AdventOfCode/2022/Day24.cs
@@ -1,172 +1,195 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
+using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AdventOfCode._2022
 {
-
     internal class Day24
     {
-        class Blizzard
-        {
-            public char Direction;
-            public (int x, int y) Location;
 
-            public Blizzard(char direction, (int x, int y) location)
-            {
-                Direction = direction;
-                Location = location;
-            }
-            public void MoveBlizzard()
-            {
+		static public (int x, int y, char d) MoveBlizzard ((int x, int y, char d) Location)
+		{
+			(int x, int y, char d) NewLocation = (0,0,'<');
+			switch (Location.d)
+			{
 
-                switch (Direction)
-                {
+				case '<':
+					{
+						if (Location.x - 1 > 0)
+						{
+							NewLocation = (Location.x - 1, Location.y, Location.d);
+						}
+						else
+						{
+							NewLocation = (width - 1, Location.y, Location.d);
+						}
+						break;
+					}
+				case '>':
+					{
+						if (Location.x + 1 < width)
+						{
+							NewLocation = (Location.x + 1, Location.y, Location.d);
+						}
+						else
+						{
+							NewLocation = (1, Location.y, Location.d);
+						}
+						break;
+					}
+				case 'v':
+					{
+						if (Location.y + 1 < height)
+						{
+							NewLocation = (Location.x, Location.y + 1, Location.d);
+						}
+						else
+						{
+							NewLocation = (Location.x, 1, Location.d);
+						}
+						break;
+					}
+				case '^':
+					{
+						if (Location.y - 1 > 0)
+						{
+							NewLocation = (Location.x, Location.y - 1, Location.d);
+						}
+						else
+						{
+							NewLocation = (Location.x, height - 1, Location.d);
+						}
+						break;
+					}
 
-                    case '<':
-                        {
-                            if (Location.x - 1 > 0)
-                            {
-                                Location = (Location.x - 1, Location.y);
-                            }
-                            else
-                            {
-                                Location = (width - 1, Location.y);
-                            }
-                            break;
-                        }
-                    case '>':
-                        {
-                            if (Location.x + 1 < width)
-                            {
-                                Location = (Location.x + 1, Location.y);
-                            }
-                            else
-                            {
-                                Location = (1, Location.y);
-                            }
-                            break;
-                        }
-                    case 'v':
-                        {
-                            if (Location.y + 1 < height)
-                            {
-                                Location = (Location.x, Location.y + 1);
-                            }
-                            else
-                            {
-                                Location = (Location.x, 1);
-                            }
-                            break;
-                        }
-                    case '^':
-                        {
-                            if (Location.y - 1 > 0)
-                            {
-                                Location = (Location.x, Location.y - 1);
-                            }
-                            else
-                            {
-                                Location = (Location.x, height - 1);
-                            }
-                            break;
-                        }
+			}
+			return NewLocation;
 
-                }
+		}
 
-            }
-        }
+		static void MoveTo((int x, int y) Destination){
+			PriorityQueue<(int x, int y, int t), int> States = new PriorityQueue<(int x, int y, int t), int>();
+			(int x, int y, int t) State = CurrentLocation;
+			//int Distance = Math.Abs(Destination.x - State.x) + Math.Abs(Destination.y - State.y);
+			States.Enqueue(State, State.t);
+			HashSet<(int x, int y, int t)> Recorded = new HashSet<(int x, int y, int t)>();
 
-        class State{
-            public static int BestResult = int.MaxValue;
-            public int Minutes;
-            public HashSet<Blizzard> AllBlizzards;
-            public(int x, int y) CurrentLocation;
-            public State(HashSet<Blizzard> Blizzards, int minutes, (int x, int y)CurrentLocation) {
-                AllBlizzards = Blizzards; 
-                Minutes= minutes;
-                this.CurrentLocation = CurrentLocation;
-            }
+			while(States.Count > 0)
+			{
+				State = States.Dequeue();
 
-        }
+				if (State.x == Destination.x && State.y == Destination.y) {
+					break;
+				}
+				if (Recorded.Contains(State))
+				{
+					continue;
+				}
+				else {
+					Recorded.Add(State);
+				}
+				HashSet<(int x, int y)> BlizzardLocations = (from Blizzard in BlizzardsStates[State.t + 1] select (Blizzard.x, Blizzard.y)).ToHashSet();
 
-        
-        static int MakeMove(State CurrentState) {
+				if (!BlizzardLocations.Contains((State.x, State.y)) && Map[State.y,State.x] == ".") 
+				{
+					//Distance = Math.Abs(Destination.x - State.x) + Math.Abs(Destination.y - State.y);
+					States.Enqueue((State.x, State.y, State.t + 1), State.t + 1);
+				}
+				if (!BlizzardLocations.Contains((State.x + 1, State.y)) && Map[State.y, State.x + 1] == ".")
+				{
+					//Distance = Math.Abs(Destination.x - (State.x + 1)) + Math.Abs(Destination.y - State.y);
+					States.Enqueue((State.x + 1, State.y, State.t + 1), State.t + 1);
+				}
+				if (!BlizzardLocations.Contains((State.x - 1, State.y)) && Map[State.y, State.x - 1] == ".")
+				{
+					//Distance = Math.Abs(Destination.x - (State.x - 1)) + Math.Abs(Destination.y - State.y);
+					States.Enqueue((State.x - 1, State.y, State.t + 1), State.t + 1);
+				}
+				if (!BlizzardLocations.Contains((State.x, State.y + 1)) && (State.y < height) && Map[State.y + 1, State.x] == ".")
+				{
+					//Distance = Math.Abs(Destination.x - State.x) + Math.Abs(Destination.y - (State.y + 1));
+					States.Enqueue((State.x, State.y + 1, State.t + 1), State.t + 1);
+				}
+				if (!BlizzardLocations.Contains((State.x, State.y - 1)) && (State.y >= 1) && Map[State.y - 1, State.x] == ".")
+				{
+					//Distance = Math.Abs(Destination.x - State.x) + Math.Abs(Destination.y  - (State.y - 1));
+					States.Enqueue((State.x, State.y - 1, State.t + 1), State.t + 1);
+				}
 
-            int Minutes = CurrentState.Minutes;
-            (int x, int y) CurrentLocation = CurrentState.CurrentLocation;
-
-            if ((CurrentLocation.x, CurrentLocation.y + 1) == Exit) {
-                return CurrentState.Minutes + 1;
-            }
-
-            if (CurrentState.Minutes > State.BestResult) {
-                return int.MaxValue;
-            }
-            HashSet<Blizzard> NewBlizzardsSate = new HashSet<Blizzard>();
-            NewBlizzardsSate.UnionWith(CurrentState.AllBlizzards);
-            
-            foreach (Blizzard Blizzard in NewBlizzardsSate) { 
-                Blizzard.MoveBlizzard();
-            }
-
-            if (!NewBlizzardsSate.Any(Bliz => Bliz.Location == (CurrentLocation.x + 1, CurrentLocation.y)) && CurrentLocation.x + 1 < width && CurrentLocation.y > 0)
-            {
-                State.BestResult = Math.Min(State.BestResult, MakeMove(new State(NewBlizzardsSate, Minutes + 1, (CurrentLocation.x + 1, CurrentLocation.y))));
-            }// Right
-
-            if (!NewBlizzardsSate.Any(Bliz => Bliz.Location == (CurrentLocation.x, CurrentLocation.y + 1)) && CurrentLocation.y + 1 < height)
-            {
-                State.BestResult = Math.Min(State.BestResult, MakeMove(new State(NewBlizzardsSate, Minutes + 1, (CurrentLocation.x, CurrentLocation.y + 1))));
-            }// Down
-
-            if (!NewBlizzardsSate.Any(Bliz => Bliz.Location == CurrentLocation) && CurrentLocation.x > 0 && CurrentLocation.y > 0) {
-                State.BestResult = Math.Min(State.BestResult,MakeMove(new State(NewBlizzardsSate, Minutes + 1, CurrentLocation)));
-            }// Not Moving
-
-            if (!NewBlizzardsSate.Any(Bliz => Bliz.Location == (CurrentLocation.x - 1, CurrentLocation.y)) && CurrentLocation.x - 1 > 0 && CurrentLocation.y > 0) {
-                State.BestResult = Math.Min(State.BestResult, MakeMove(new State(NewBlizzardsSate, Minutes + 1, (CurrentLocation.x - 1, CurrentLocation.y))));
-            } // Left
-
-            if (!NewBlizzardsSate.Any(Bliz => Bliz.Location == (CurrentLocation.x, CurrentLocation.y - 1)) && CurrentLocation.y - 1 > 0) 
-            {
-                State.BestResult = Math.Min(State.BestResult, MakeMove(new State(NewBlizzardsSate, Minutes + 1, (CurrentLocation.x , CurrentLocation.y - 1))));
-            } // UP
+			}
+			CurrentLocation = State;
+		}
 
 
-            return State.BestResult;
-        }
-
-        static int width;
-        static int height;
-        static (int x, int y) Exit;
-        static string Path = ".\\2022\\Input\\InputDay24.txt";
+		static (int x, int y, int t) CurrentLocation;
+		static int width;
+		static int height;
+		static List<List<(int x, int y, char d)>> BlizzardsStates = new List<List<(int x, int y, char d)>>();
+        static string[,] Map;
+		static (int x, int y) Exit;
+		static (int x, int y) Start;
+		static string Path = ".\\2022\\Input\\InputDay24.txt";
         static string Data = File.ReadAllText(Path);
         public static int Part1()
         {
-            int answer = 0;
-            
-            Char[] BlizardTypes = new char[] { '<', '>', '^', 'v' };
-            HashSet<Blizzard> StartBlizzardState= new HashSet<Blizzard>();
-            
             string[] lines = Data.Split("\r\n");
-            Exit = (lines[0].Length - 2, lines.Count() - 1);
-            width= lines[0].Length - 1;
-            height= lines.Count() - 1;
-            for (int y = 0; y < lines.Length; y++) { 
-                for (int x = 0; x < lines[0].Length; x++) {
-                    if (BlizardTypes.Contains(lines[y][x])) {
-                        StartBlizzardState.Add(new Blizzard(lines[y][x], (x, y)));
+            Map = new string[lines.Length,lines[0].Length];
+			width = lines[0].Length - 1;
+			height = lines.Count() - 1;
+			CurrentLocation = (1, 0, 0);
+			Exit = (lines[0].Length - 2, lines.Count() - 1);
+			Start = (1, 0);
+
+			for (int y = 0; y < lines.Length; y++) {
+                for (int x = 0; x < lines[0].Length; x++)
+                {
+                    if (lines[y][x] == '.' || lines[y][x] == '#')
+                    {
+                        Map[y, x] = lines[y][x].ToString();
+                    }
+                    else {
+                        Map[y, x] = ".";
                     }
                 }
+
             }
-            State StartState = new State(StartBlizzardState, 0, (1,0));
-            answer = MakeMove(StartState);
-            
+
+			List<(int x, int y, char d)> StartBlizzardsState = new List<(int x, int y, char d)>();
+
+			for (int y = 0; y < lines.Length; y++)
+			{
+				for (int x = 0; x < lines[0].Length; x++)
+				{
+					if (lines[y][x] != '.' && lines[y][x] != '#')
+					{
+                        StartBlizzardsState.Add((x, y, lines[y][x]));
+					}
+				
+				}
+
+			}
+            BlizzardsStates.Add(StartBlizzardsState);
+
+
+            for(int t = 1; t < 1400; t++)
+            {
+				List<(int x, int y, char d)> BlizzardsState = new List<(int x, int y, char d)>();
+				foreach ((int x, int y, char d) Blizzard in BlizzardsStates[t-1]) {
+					BlizzardsState.Add(MoveBlizzard(Blizzard));	
+				}
+				BlizzardsStates.Add(BlizzardsState);
+
+			}
+			MoveTo(Exit);
+
+
+			int answer = CurrentLocation.t;
 
             return answer;
         }
@@ -174,7 +197,10 @@ namespace AdventOfCode._2022
    
         public static int Part2()
         {
-            int answer = 0;
+			MoveTo(Start);
+			MoveTo(Exit);
+
+            int answer = CurrentLocation.t;
 
             return answer;
         }

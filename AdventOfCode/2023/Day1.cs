9f0ee22f6da383b39b61b2562518eebbc0be68f7
Bas de Koning <bas.dekoning@ogd.nl>
2023-12-01 09:27:48 +0100

    Day 1 working

--- AdventOfCode/2023/Day1.cs
+++ AdventOfCode/2023/Day1.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Collections;
 
 namespace AdventOfCode
 {
@@ -16,7 +17,7 @@ namespace AdventOfCode
             {
                 return str;
             }
-            str = str.Substring(0, position) + replace + str.Substring(position + term.Length);
+            str = str.Substring(0, position) + replace + str.Substring(position + 1);
             return str;
         }
         public static Int64 Part1() 
@@ -76,40 +77,40 @@ namespace AdventOfCode
             foreach (string line in lines) {
                 string parsedline = line;
 
-                for (int x = 0; x < line.Count(); x++) {
+                for (int x = 0; x < line.Length; x++) {
 
                     switch (line[x]) {
 
 
 
                         case 'o':
-                            if (x + 2 <= line.Count() - 1)
+                            if (x + 2 <= line.Length - 1)
                             {
                                 if (line[x + 1] == 'n' && line[x + 2] == 'e')
                                 {
                                     parsedline = ReplaceFirst(parsedline, "one", "1");
-                                    x = x + 2;
+                                    //x = x + 2;
                                 }
                             }
 
                             break;
                         case 't':
-                            if (x + 2 <= line.Count() - 1)
+                            if (x + 2 <= line.Length - 1)
                             {
                                 if (line[x + 1] == 'w' && line[x + 2] == 'o')
                                 {
                                     parsedline = ReplaceFirst(parsedline, "two", "2");
                                     //parsedline = parsedline.Replace("two", "2");
-                                    x = x + 2;
+                                    //x = x + 2;
                                     break;
                                 }
                             }
-                            if (x + 4 <= line.Count() - 1)
+                            if (x + 4 <= line.Length - 1)
                             {
                                 if (line[x + 1] == 'h' && line[x + 2] == 'r' && line[x + 3] == 'e' && line[x + 4] == 'e')
                                 {
                                     parsedline = ReplaceFirst(parsedline, "three", "3");
-                                    x = x + 4;
+                                    //x = x + 4;
                                     break;
                                 }
                             }
@@ -117,62 +118,62 @@ namespace AdventOfCode
 
 
                         case 'f':
-                            if (x + 3 <= line.Count() - 1)
+                            if (x + 3 <= line.Length - 1)
                             {
                                 if (line[x + 1] == 'o' && line[x + 2] == 'u' && line[x + 3] == 'r')
                                 {
                                     parsedline = ReplaceFirst(parsedline, "four", "4");
-                                    x = x + 3;
+                                    //x = x + 3;
                                     break;
                                 }
                             }
-                            if (x + 3 <= line.Count() - 1)
+                            if (x + 3 <= line.Length - 1)
                             {
                                 if (line[x + 1] == 'i' && line[x + 2] == 'v' && line[x + 3] == 'e')
                                 {
                                     parsedline = ReplaceFirst(parsedline, "five", "5");
-                                    x = x + 3;
+                                    //x = x + 3;
                                     break;
                                 }
                             }
                             break;
                         case 's':
-                            if (x + 2 <= line.Count() - 1)
+                            if (x + 2 <= line.Length - 1)
                             {
                                 if (line[x + 1] == 'i' && line[x + 2] == 'x')
                                 {
                                     parsedline = ReplaceFirst(parsedline, "six", "6");
-                                    x = x + 2;
+                                    //x = x + 2;
                                     break;
                                 }
                             }
-                            if (x + 4 <= line.Count() - 1)
+                            if (x + 4 <= line.Length - 1)
                             {
                                 if (line[x + 1] == 'e' && line[x + 2] == 'v' && line[x + 3] == 'e' && line[x + 4] == 'n')
                                 {
                                     parsedline = ReplaceFirst(parsedline, "seven", "7");
-                                    x = x + 4;
+                                    //x = x + 4;
                                     break;
                                 }
                             }
                             break;
                         case 'e':
-                            if (x + 4 <= line.Count() - 1)
+                            if (x + 4 <= line.Length - 1)
                             {
                                 if (line[x + 1] == 'i' && line[x + 2] == 'g' && line[x + 3] == 'h' && line[x + 4] == 't')
                                 {
                                     parsedline = ReplaceFirst(parsedline, "eight", "8");
-                                    x += 4;
+                                    //x += 4;
                                 }
                             }
                             break;
                         case 'n':
-                            if (x + 3 <= line.Count() - 1)
+                            if (x + 3 <= line.Length - 1)
                             {
                                 if (line[x + 1] == 'i' && line[x + 2] == 'n' && line[x + 3] == 'e')
                                 {
                                     parsedline = ReplaceFirst(parsedline, "nine", "9");
-                                    x = x + 3;
+                                    //x = x + 3;
                                 }
                             }
                             break;
@@ -187,6 +188,17 @@ namespace AdventOfCode
 
                 parsedlines.Add(parsedline);
             }
+
+            using (StreamWriter outputfile = new StreamWriter(".\\2023\\Input\\23output1.txt"))
+            {
+                foreach (string line in parsedlines)
+                {
+                   
+                    outputfile.WriteLine(line);
+                   
+                }
+            }
+
 
             foreach (string line in parsedlines)
             {

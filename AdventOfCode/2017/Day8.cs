d03236d87cbf4fc6c4a423033e47f6159c607bde
Bas de Koning <bas.dekoning@gmail.com>
2023-12-08 14:47:13 +0100

    Day8/ Day9 2017

---
+++ AdventOfCode/2017/Day8.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    internal class Y2017D8
+    {
+        static public Dictionary<string, int> Registers = new Dictionary<string, int>();
+        public static string[] lines = File.ReadAllLines(".\\2017\\Input\\inputDay8.txt");
+        static int MaxValue = 0;
+
+        static public bool TestOperation (string Register, string test, int value){
+            bool Result = false;
+
+            switch (test) {
+
+                case ">":
+                    if (Registers[Register] > value)
+                    {
+                        Result = true;
+                    }
+                    break;
+                case "<":
+                    if (Registers[Register] < value)
+                    {
+                        Result = true;
+                    }
+                    break;
+                case "==":
+                    if (Registers[Register] == value)
+                    {
+                        Result = true;
+                    }
+                    break;
+                case "!=":
+                    if (Registers[Register] != value)
+                    {
+                        Result = true;
+                    }
+                    break;
+                case ">=":
+                    if (Registers[Register] >= value) { 
+                        Result = true;
+                    }
+                    break;
+                case "<=":
+                    if (Registers[Register] <= value) { 
+                        Result = true;
+                    }
+                    break;
+            
+            }
+            return Result;
+        }
+
+        public int Part1() 
+        {
+              
+            int answer = 0;
+            foreach (string line in lines) {
+                string register = line.Split(" ")[0];
+                if (!Registers.ContainsKey(register))
+                {
+                    Registers.Add(register, 0);
+                }
+            }
+            foreach(string line in lines)
+            {
+                string[] Parts = line.Split(" ");
+                string Register = Parts[0];
+                string Operation = Parts[1];
+                int OperationValue = int.Parse(Parts[2]);
+                string TestRegister = Parts[4];
+                string Test = Parts[5];
+                int TestValue = int.Parse(Parts[6]);
+
+                if (TestOperation(TestRegister, Test, TestValue)) {
+                    switch (Operation) {
+                        case "inc":
+                            Registers[Register] += OperationValue;
+                            break;
+                        case "dec":
+                            Registers[Register] -= OperationValue;
+                            break;
+    
+                    }
+                }
+
+                if(Registers.Values.Max() > MaxValue) { 
+                    MaxValue = Registers.Values.Max();
+                }
+
+            }
+
+            answer = Registers.Values.Max();
+
+            return answer;
+        }
+
+        public int Part2()
+        {
+
+            int answer = MaxValue;
+
+
+
+            return answer;
+        }
+
+    }
+}
---
+++ AdventOfCode/2017/Day9.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    internal class Y2017D9
+    {
+        static public int GarbageCounter = 0;
+        public static string Text = File.ReadAllText(".\\2017\\Input\\inputDay9.txt");
+        public int Part1() 
+        {
+            int answer = 0;
+            int Depth = 0;
+            bool Garbage = false;
+            for (int x = 0; x < Text.Length; x++) { 
+                char c = Text[x];
+                if (c == '!') {
+                    x++;
+                    continue;
+                }
+
+                switch (c)
+                {
+                    case '{':
+                        if (Garbage == false)
+                        {
+                            Depth++;
+                            continue;
+                        }
+                        break;
+                    case '}':
+                        if (Garbage == false)
+                        {
+                            answer += Depth;
+
+                            Depth--;
+
+                            continue;
+                        }
+                        break;
+
+                    case '<':
+                        if (Garbage == false)
+                        {
+                            Garbage = true;
+                            continue;
+                        }
+                        break;
+                    case '>':
+                       
+                        Garbage = false;
+                        continue;
+                }
+                if (Garbage == true)
+                {
+                    GarbageCounter++;
+                }
+            }
+
+            return answer;
+        }
+
+        public int Part2()
+        {
+            int answer = 0;
+
+            answer = GarbageCounter;
+
+
+            return answer;
+        }
+
+    }
+}

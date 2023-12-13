e92fa080113819521fc9103c425baec70611027e
Bas de Koning <bas.dekoning@ogd.nl>
2023-12-13 09:20:18 +0100

    synced

--- AdventOfCode/2023/Day13.cs
+++ AdventOfCode/2023/Day13.cs
@@ -11,32 +11,61 @@ namespace AdventOfCode
     {
         static List<List<string>> Patterns = new List<List<string>>();
         public static string[] lines = File.ReadAllLines(".\\2023\\Input\\inputDay13.txt");
-
+        bool part2 = false;
         bool TestHorizontal(List<string> Pattern, int FoundPossibleRow) {
+            bool SmudgeFixed = false;
+            int up = FoundPossibleRow;
+            int down = FoundPossibleRow +1;
 
-            int up = FoundPossibleRow - 1;
-            int down = FoundPossibleRow + 2;
-
-            while(up > 0 && down < Pattern.Count) {
+            while(up >= 0 && down < Pattern.Count) {
 
                 if (Pattern[up] != Pattern[down]) {
+                    
+                    if (part2 && CompareString(Pattern[up], Pattern[down]) && SmudgeFixed == false )
+                    {
+                        SmudgeFixed = true;
+                        up--;
+                        down++;
+                        continue;
+                    }
+
                     return false;
                 }
                 up--;
                 down++;
             }
-
+            if (part2) { return SmudgeFixed; }
             return true;
 
 
             
         }
+        bool CompareString(string s1, string s2)
+        {
+            int counter = 0;
+            for (int x = 0; x < s1.Length; x++) {
+                if (s1[x] != s2[x]) { 
+                    counter++;
+                }
+            }
+
+            if(counter ==1)
+            {
+                return true;
+            }
+            else{
+                return false;
+            }
+
+           
+        } 
 
         bool TestVertical(List<string> Pattern, int FoundPossibleColumn)
         {
-            int Left = FoundPossibleColumn - 1;
-            int Right = FoundPossibleColumn + 2;
-            while (Left > 0 && Right < Pattern[0].Length)
+            int Left = FoundPossibleColumn;
+            int Right = FoundPossibleColumn +1;
+            bool SmudgeFixed = false;
+            while (Left >= 0 && Right < Pattern[0].Length)
             {
 
 
@@ -50,6 +79,12 @@ namespace AdventOfCode
                 }
                 if (ColumnA != ColumnB)
                 {
+                    if (part2 && CompareString(ColumnA, ColumnB) && SmudgeFixed == false){
+                        SmudgeFixed = true;
+                        Left--;
+                        Right++;
+                        continue;
+                    }
                     return false;
                 }
 
@@ -57,7 +92,7 @@ namespace AdventOfCode
                 Right++;
             }
 
-
+            if (part2) { return SmudgeFixed; }
             return true;
            
         }
@@ -98,6 +133,8 @@ namespace AdventOfCode
                     }
                     if (ColumnA == ColumnB)
                     {
+                        
+
                         FoundPossibeColumns.Add(x);
                         
                         
@@ -116,7 +153,7 @@ namespace AdventOfCode
                 foreach (int FoundPossibeColumn in FoundPossibeColumns) {
                     if (TestVertical(TestPattern, FoundPossibeColumn)){
                         answer += (FoundPossibeColumn+1);
-                        Console.WriteLine("Found Reflection on Column: " + FoundPossibeColumn + "/" + (FoundPossibeColumn + 1));
+                        //Console.WriteLine("Found Reflection on Column: " + FoundPossibeColumn + "/" + (FoundPossibeColumn + 1));
                         Found = true;
                         break;
                     }
@@ -127,7 +164,7 @@ namespace AdventOfCode
                     {
                         if(TestHorizontal(TestPattern, FoundPossibleRow))
                         {
-                            Console.WriteLine("Found Reflection on Rows: " + FoundPossibleRow + "/" + (FoundPossibleRow + 1));
+                            //Console.WriteLine("Found Reflection on Rows: " + FoundPossibleRow + "/" + (FoundPossibleRow + 1));
                             answer += (FoundPossibleRow+1)*100;
                         }
                     }
@@ -145,10 +182,88 @@ namespace AdventOfCode
         public int Part2()
         {
             int answer = 0;
+            part2 = true;
+            List<string> Pattern = new List<string>();
+            foreach (string line in lines)
+            {
+
+                if (line == "")
+                {
+                    Patterns.Add(Pattern);
+                    Pattern = new List<string>();
+                }
+                else
+                {
+                    Pattern.Add(line);
+
+                }
+            }
+            Patterns.Add(Pattern);
+            int i = 0;
+            foreach (List<string> TestPattern in Patterns)
+            {
+                List<int> FoundPossibleColumns = new List<int>();
+                List<int> FoundPossibleRows = new List<int>();
+                for (int x = 0; x < TestPattern[0].Length - 1; x++)
+                {
+
+                    string ColumnA = "";
+                    string ColumnB = "";
+
+                    for (int y = 0; y < TestPattern.Count; y++)
+                    {
+                        ColumnA += TestPattern[y][x];
+                        ColumnB += TestPattern[y][x + 1];
+                    }
+                    if (ColumnA == ColumnB)
+                    {
 
 
+                        FoundPossibleColumns.Add(x);
+                    }
+                    else if (CompareString(ColumnA, ColumnB)) { 
+                        FoundPossibleColumns.Add(x);
+                    }
+                }
 
+                for (int y = 0; y < TestPattern.Count - 1; y++)
+                {
+                    if (TestPattern[y] == TestPattern[y + 1])
+                    {
+                        FoundPossibleRows.Add(y);
 
+                    }    //goto next;
+                    else if (CompareString(TestPattern[y], TestPattern[y + 1]))
+                    {
+                        FoundPossibleRows.Add(y);
+                    }
+                }
+                bool Found = false;
+                foreach (int FoundPossibeColumn in FoundPossibleColumns)
+                {
+                    if (TestVertical(TestPattern, FoundPossibeColumn))
+                    {
+                        answer += (FoundPossibeColumn + 1);
+                        Console.WriteLine("Found Reflection on Column: " + FoundPossibeColumn + "/" + (FoundPossibeColumn + 1));
+                        Found = true;
+                        break;
+                    }
+                }
+                if (!Found)
+                {
+                    foreach (int FoundPossibleRow in FoundPossibleRows)
+                    {
+                        if (TestHorizontal(TestPattern, FoundPossibleRow))
+                        {
+                            Console.WriteLine("Found Reflection on Rows: " + FoundPossibleRow + "/" + (FoundPossibleRow + 1));
+                            answer += (FoundPossibleRow + 1) * 100;
+                            Found = true;
+                        }
+                    }
+                }
+                if (!Found) { Console.WriteLine("Error not found"); }
+
+            }
             return answer;
         }
 

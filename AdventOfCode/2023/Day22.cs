d13624304b35d9159933b2a79f8961616d94d372
Bas de Koning <bas.dekoning@gmail.com>
2023-12-22 13:16:22 +0100

    Day22 done

--- AdventOfCode/2023/Day22.cs
+++ AdventOfCode/2023/Day22.cs
@@ -15,11 +15,11 @@ namespace AdventOfCode
         static int Width = Lines[0].Length;
         static List<Brick> Bricks = new();
         static Dictionary<int, List<int>> SupportList = new Dictionary<int, List<int>>();
+        static Dictionary<int, List<int>> SupportedBy = new Dictionary<int, List<int>>();
         class Brick : IComparable<Brick>
         {
             public (int x, int y, int z) coordinate1;
             public (int x, int y, int z) coordinate2;
-            int Supports = 0;
             public int id;
 
             public Brick((int x, int y, int z) coordinate1, (int x, int y, int z) coordinate2, int id)
@@ -53,8 +53,8 @@ namespace AdventOfCode
                             {
                                 if (b.coordinate1.x <= x && b.coordinate2.x >= x  && b.coordinate1.y <= y && b.coordinate2.y >= y) { 
                                     Canfall = false;
-                                    b.Supports++;
                                     SupportList[b.id].Add(this.id);
+                                    SupportedBy[this.id].Add(b.id);
                                     goto nextBlock;
                                 }
                             }
@@ -74,6 +74,16 @@ namespace AdventOfCode
 
         }
         
+        static public void FallingBlocks(HashSet<int> Falling, int id)
+        {
+            foreach (var i in SupportList[id].Where(i => !SupportedBy[i].Except(Falling).Any()))
+            {
+                Falling.Add(i);
+                FallingBlocks(Falling, i);
+            }
+        }
+
+
         public int Part1() 
         {
             int answer = 0;
@@ -87,6 +97,7 @@ namespace AdventOfCode
                 (int x, int y, int z) Coo2 = (temp[0], temp[1], temp[2]);
                 Bricks.Add(new(Coo1, Coo2, id));
                 SupportList.Add(id, []);
+                SupportedBy.Add(id, []);
                 id++;
                 
 
@@ -101,32 +112,22 @@ namespace AdventOfCode
                 b.CanMove();
             }
 
-
             foreach (var b in Bricks) {
                 if (SupportList[b.id].Count == 0)
                 {
                     answer++;
                     continue;
                 }
-               
-                foreach (var brick in SupportList[b.id])
+                bool CanBeDestroyed = true;
+                foreach (var SupportBrick in SupportList[b.id])
                 {
-                    int count = 0;
-                    foreach (var list in SupportList.Values)
-                    {
-                        if (list.Contains(brick))
-                        {
-                            count++;
-                        }
-                    }
-                    if(count == 1)
-                    {
-                        goto NextBrick;
-                    }
-                }
-                answer++;
 
-            NextBrick:;
+                    if (SupportedBy[SupportBrick].Count == 1)
+                    {
+                        CanBeDestroyed = false;
+                    }                    
+                }
+                if (CanBeDestroyed) { answer++; }
             }
 
             return answer;
@@ -135,9 +136,12 @@ namespace AdventOfCode
         public int Part2()
         {
             int answer = 0;
-
-
-
+            foreach(var b in Bricks)
+            {
+                HashSet<int> Falling = new([ b.id ]);
+                FallingBlocks(Falling, b.id);
+                answer += Falling.Count() - 1;
+            }
 
             return answer;
         }

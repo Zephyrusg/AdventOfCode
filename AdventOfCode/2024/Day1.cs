d8f6b6572befb47ef756248cff7d608c070f9552
Bas de Koning <bas.dekoning@ogd.nl>
2024-12-01 06:44:27 +0100

    Day1 2024 done

---
+++ AdventOfCode/2024/Day1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace AdventOfCode
+{
+    internal class Y2024D1
+    {
+        public static string[] Lines = File.ReadAllLines(".\\2024\\Input\\inputDay1.txt");
+        public static List<int> ListA = new();
+        public static List<int> ListB = new();
+        public int Part1() 
+        {
+            int answer = 0;
+           
+            
+            foreach(string line in Lines)
+            {
+                int[]parts = line.Split("   ").Select(u => int.Parse(u)).ToArray();
+                ListA.Add(parts[0]);
+                ListB.Add(parts[1]);
+
+            }
+            ListA.Sort();
+            ListB.Sort();
+            for(int x =0; x < ListA.Count(); x++)
+            {
+                answer += Math.Abs(ListA[x] - ListB[x]);
+            }
+
+            return answer;
+        }
+
+        public int Part2()
+        {
+            int answer = 0;
+            for (int x = 0; x < ListA.Count(); x++)
+            {
+                answer += ListB.Where(n => n == ListA[x]).Count() * ListA[x];
+            }
+
+            return answer;
+        }
+
+    }
+}
---
+++ AdventOfCode/2024/Template.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    internal class Y2024D0
+    {
+        public static string[] Lines = File.ReadAllLines(".\\2023\\Input\\inputDayX.txt");
+        
+        public int Part1() 
+        {
+            int answer = 0;
+
+       
+
+
+            return answer;
+        }
+
+        public int Part2()
+        {
+            int answer = 0;
+
+
+
+
+            return answer;
+        }
+
+    }
+}

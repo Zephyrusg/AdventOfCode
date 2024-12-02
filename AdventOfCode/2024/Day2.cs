62f3f637362618161cc889705168d17068bc83f8
Bas de Koning <bas.dekoning@ogd.nl>
2024-12-02 12:06:54 +0100

    Day 2 works

--- AdventOfCode/2024/Day2.cs
+++ AdventOfCode/2024/Day2.cs
@@ -10,164 +10,48 @@ namespace AdventOfCode
     internal class Y2024D2
     {
         public static string[] Lines = File.ReadAllLines(".\\2024\\Input\\inputDay2.txt");
-        public List<int[]> reports = new();
-        static List<int[]> UnSafeReports = new();
+        public List<List<int>> reports = new();
+        static List<List<int>> UnSafeReports = new();
         static int answer = 0;
+
+        public bool IsSafe(List<int> report)
+        {
+
+            if (report[0] < report[1])
+            {
+
+                for (int x = 0; x < report.Count() - 1; x++)
+                {
+                    if (!(report[x] < report[x + 1] && (report[x + 1] - report[x] <= 3) && (report[x + 1] - report[x] > 0)))
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+
+                for (int x = 0; x < report.Count() - 1; x++)
+                {
+                    if (!(report[x] > report[x + 1] && (report[x] - report[x + 1] <= 3) && (report[x] - report[x + 1] > 0)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+
+        }
         public int Part1() 
         {
             foreach(string line in Lines)
             {
-                int[] parts = line.Split(" ").Select(l => int.Parse(l)).ToArray();
+                List<int> parts = line.Split(" ").Select(l => int.Parse(l)).ToList();
                 reports.Add(parts);
             }
 
-            foreach (int[] report in reports)
-            {
-                bool safe = true;
-                if (report[0] < report[1] ){
-
-                    for (int x = 0; x < report.Count() - 1; x++)
-                    {
-                        if (!(report[x] < report[x+1] && (report[x+1] - report[x] <= 3) && (report[x + 1] - report[x] > 0)))
-                        {
-                            safe = false;
-                            UnSafeReports.Add(report);
-                            break;
-                        }
-                    }
-                }
-                else
-                {
-
-                    for (int x = 0; x < report.Count() - 1; x++)
-                    {
-                        if (!(report[x] > report[x + 1] && (report[x] - report[x + 1] <= 3) && (report[x] - report[x + 1] > 0)))
-                        {
-                            safe = false;
-                            UnSafeReports.Add(report);
-                            break;
-                        }
-                    }
-                }
-                if (safe)
-                {
-                    answer++;
-                }
-
-            }
-       
-
-
-            return answer;
-        }
-
-        public int Part2()
-        {
-            List<List<int>> FixedReports = new();
-            foreach (int[] report in UnSafeReports)
-            {
-                List<int> failedlist = report.ToList();
-                bool safe = true;
-                // Deja vu
-                if (report[0] == report[1])
-                {
-                    failedlist.RemoveAt(0);
-                    FixedReports.Add(failedlist);
-
-                }
-                else if (report[0] < report[1])
-                {
-
-                    for (int x = 0; x < report.Count() - 1; x++)
-                    {
-                        if (!(report[x] < report[x + 1] && (report[x + 1] - report[x] <= 3) && (report[x + 1] - report[x] > 0)))
-                        {
-                            // Wrong direction by first number
-                            if(x == 1 && report[x ] > report[x + 1] && report[x + 1] > report[x + 2]){
-                                failedlist.RemoveAt(x-1);
-                            }
-                            // At the end drop it
-                            else if (x + 1 == report.Count() - 1)
-                            {
-                                failedlist.RemoveAt(x + 1);
-                            }
-                            // Is this current number faulty
-                            else if (x > 0 && report[x - 1] < report[x + 1] && (report[x + 1] - report[x - 1] <= 3) && (report[x + 1] - report[x - 1] > 0))
-                            {
-                                failedlist.RemoveAt(x);
-                            }
-                            // Is this next number faulty
-                            else if (report[x] < report[x + 2] && (report[x+2] - report[x] <= 3) && (report[x + 2] - report[x] > 0))
-                            {
-                                failedlist.RemoveAt(x+1);
-                            }
-                            //To large steps
-                            else if (report[x+1] - report[x] >= 4)
-                            {
-                                failedlist.RemoveAt(x);
-                            }
-                            //catch all
-                            else
-                            {
-                                failedlist.RemoveAt(x + 1);
-                            }
-                            
-                            FixedReports.Add(failedlist);
-                            break;
-
-                        }
-
-                    }
-                }
-                else
-                {
-
-                    for (int x = 0; x < report.Count() - 1; x++)
-                    {
-                        if (!(report[x] > report[x + 1] && (report[x] - report[x + 1] <= 3) && (report[x] - report[x + 1] > 0)))
-                        {
-                            // Wrong direction by first number
-                            if (x == 1 && report[x] < report[x + 1] && report[x + 1] < report[x + 2])
-                            {
-                                failedlist.RemoveAt(x-1);
-                            }
-                            // At the end drop it
-                            else if (x + 1 == report.Count() - 1)
-                            {
-                                failedlist.RemoveAt(x + 1);
-                            }
-                            // Is this next number faulty
-                            else if (x > 0 && report[x - 1] > report[x + 1] && (report[x - 1] - report[x + 1] <= 3) && (report[x - 1] - report[x + 1] > 0))
-                            {
-                                failedlist.RemoveAt(x);
-                            }
-                            // Is this next number faulty
-                            else if (report[x] > report[x + 2] && (report[x] - report[x+2] <= 3) && (report[x ] - report[x+ 2] > 0))
-                            {
-                                failedlist.RemoveAt(x + 1);
-                            }
-                            //To large steps
-                            else if (report[x] - report[x + 1] >= 4)
-                            {
-                                failedlist.RemoveAt(x);
-                            }
-                            //catch all
-                            else
-                            {
-                                
-                                failedlist.RemoveAt(x + 1);
-                            }
-
-                            FixedReports.Add(failedlist);
-                            break;
-
-                        }
-                    }
-                }
-
-            }
-
-            foreach (List<int> report in FixedReports)
+            foreach (List<int> report in reports)
             {
                 bool safe = true;
                 if (report[0] < report[1])
@@ -177,8 +61,88 @@ namespace AdventOfCode
                     {
                         if (!(report[x] < report[x + 1] && (report[x + 1] - report[x] <= 3) && (report[x + 1] - report[x] > 0)))
                         {
-
                             safe = false;
+                            UnSafeReports.Add(report);
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+
+                    for (int x = 0; x < report.Count() - 1; x++)
+                    {
+                        if (!(report[x] > report[x + 1] && (report[x] - report[x + 1] <= 3) && (report[x] - report[x + 1] > 0)))
+                        {
+                            safe = false;
+                            UnSafeReports.Add(report);
+                            break;
+                        }
+                    }
+                }
+                if (safe)
+                {
+                    answer++;
+                }
+
+            }
+
+            return answer;
+        }
+
+        public int Part2()
+        {
+            List<List<int>> FixedReports = new();
+            foreach (List<int> report in UnSafeReports)
+            {
+                List<int> failedlist = report.ToList();
+                bool safe = true;
+                // Deja vu
+                if (report[0] == report[1])
+                {
+                    failedlist.RemoveAt(0);
+                    if (IsSafe(failedlist))
+                    {
+                        answer++;
+                    }
+
+                }
+                else if (report[0] < report[1])
+                {
+
+                    for (int x = 0; x < report.Count() - 1; x++)
+                    {
+                        if (!(report[x] < report[x + 1] && (report[x + 1] - report[x] <= 3) && (report[x + 1] - report[x] > 0)))
+                        {
+                            
+                            List<int> failedlist1 = report.ToList();
+                            List<int> failedlist2 = report.ToList();
+                            if (x > 0)
+                            {
+                                List<int> failedlist3 = report.ToList();
+                                failedlist3.RemoveAt(x - 1);
+                                if (IsSafe(failedlist3))
+                                {
+                                    //FixedReports.Add(failedlist3);
+                                    answer++;
+                                    break;
+                                }
+
+                            }
+                            failedlist1.RemoveAt(x);
+                            if (IsSafe(failedlist1))
+                            {
+                                //FixedReports.Add(failedlist1);
+                                answer++;
+                                break;
+                            }
+                            failedlist2.RemoveAt(x + 1);
+                            if (IsSafe(failedlist2))
+                            {
+                                //FixedReports.Add(failedlist2);
+                                answer++;
+                                break;
+                            }
                             break;
 
                         }
@@ -192,26 +156,43 @@ namespace AdventOfCode
                     {
                         if (!(report[x] > report[x + 1] && (report[x] - report[x + 1] <= 3) && (report[x] - report[x + 1] > 0)))
                         {
+                           
+                            List<int> failedlist1 = report.ToList();
+                            List<int> failedlist2 = report.ToList();
+                            if (x > 0)
+                            {
+                                List<int> failedlist3 = report.ToList();
+                                failedlist3.RemoveAt(x - 1);
+                                if (IsSafe(failedlist3))
+                                {
+                                    //FixedReports.Add(failedlist3);
+                                    answer++;
+                                    break;
+                                }
 
-                            safe = false;
+                            }
+                            failedlist1.RemoveAt(x);
+                            if (IsSafe(failedlist1))
+                            {
+                                //FixedReports.Add(failedlist1);
+                                answer++;
+                                break;
+                            }
+                            failedlist2.RemoveAt(x + 1);
+                            if (IsSafe(failedlist2))
+                            {
+                                //FixedReports.Add(failedlist2);
+                                answer++;
+                                break;
+                            }
+
                             break;
 
                         }
                     }
                 }
-
-
-                if (safe)
-                {
-                    answer++;
-                }
+                               
             }
-
-
-
-            
-
-
             return answer;
         }
 
